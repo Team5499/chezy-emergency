0a42d08ab2dbd85baee6b56cf4dff7c7bce8f6a7
Rafie Walker <slickytail.mc@gmail.com>
2016-09-25 12:13:09 -0700

    Added C:

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,24 @@ ROBOTCOMMAND=$3
 TARGET_USER=lvuser
 TARGET_DIR=/home/lvuser
 # Probe for connection
-TARGET="roborio-$TEAM_NUMBER-frc.local..."
-echo "Probing for $TARGET"
-ssh "$USER@$TARGET" true &> /dev/null
+TARGET="roborio-$TEAM_NUMBER-frc.local"
+echo "Probing for $TARGET..."
+#ssh "$USER@$TARGET" true &> /dev/null
+
+PROGRAM=$(sed 's/C://g' <<< $PROGRAM)
+
+ping $TARGET &> /dev/null
 if [[ $? -eq 0 ]]; then
     echo "Removing old program..."
-    ssh "TARGET_USER@TARGET" "rm -f $TARGET_DIR/FRCUserProgram"
+    ssh "$TARGET_USER@$TARGET" "rm -f $TARGET_DIR/FRCUserProgram"
     echo "Copying over new program..."
-    scp "$PROGRAM" "$TARGET_USER@TARGET:$TARGET_DIR/FRCUserProgram"
+    scp "$PROGRAM" "$TARGET_USER@$TARGET:$TARGET_DIR/FRCUserProgram"
     echo "Stoping netconsole-host..."
-    ssh ssh "TARGET_USER@TARGET" "killall -q netconsole-host || :"
+    ssh ssh "$TARGET_USER@$TARGET" "killall -q netconsole-host || :"
     echo "Copying over robotCommand..." 
-    scp "$ROBOTCOMMAND" "$TARGET_USER@TARGET:$TARGET_DIR"
+    scp "$ROBOTCOMMAND" "$TARGET_USER@$TARGET:$TARGET_DIR"
     echo "Cleaning up..."
-    ssh "TARGET_USER@TARGET" ". /etc/profile.d/natinst-path.sh;
+    ssh "$TARGET_USER@$TARGET" ". /etc/profile.d/natinst-path.sh;
                               chmod a+x $TARGET_DIR/FRCUserProgram;
                               /usr/local/frc/bin/frcKillRobot.sh -t -r;
                               sync"
@@ -31,15 +35,15 @@ echo "Not found - probing for $TARGET..."
 ssh "$USER@$TARGET" true &> /dev/null
 if [[ $? -eq 0 ]]; then
     echo "Removing old program..."
-    ssh "TARGET_USER@TARGET" "rm -f $TARGET_DIR/FRCUserProgram"
+    ssh "$TARGET_USER$@$TARGET" "rm -f $TARGET_DIR/FRCUserProgram"
     echo "Copying over new program..."
-    scp "$PROGRAM" "$TARGET_USER@TARGET:$TARGET_DIR/FRCUserProgram"
+    scp "$PROGRAM" "$TARGET_USER@$TARGET:$TARGET_DIR/FRCUserProgram"
     echo "Stoping netconsole-host..."
-    ssh ssh "TARGET_USER@TARGET" "killall -q netconsole-host || :"
+    ssh ssh "$TARGET_USER@$TARGET" "killall -q netconsole-host || :"
     echo "Copying over robotCommand..." 
-    scp "$ROBOTCOMMAND" "$TARGET_USER@TARGET:$TARGET_DIR"
+    scp "$ROBOTCOMMAND" "$TARGET_USER@$TARGET:$TARGET_DIR"
     echo "Cleaning up..."
-    ssh "TARGET_USER@TARGET" ". /etc/profile.d/natinst-path.sh;
+    ssh "$TARGET_USER@$TARGET" ". /etc/profile.d/natinst-path.sh;
                               chmod a+x $TARGET_DIR/FRCUserProgram;
                               /usr/local/frc/bin/frcKillRobot.sh -t -r;
                               sync"
